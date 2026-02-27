ef8a2f2aead803aa3f83d763f86a5f09ce3da5b8
Aminu Ambursa <39995410+princehaifan@users.noreply.github.com>
2026-02-27 12:19:31 +0000

    Create board.py with Space and Board classes

--- board.py
+++ board.py
@@ -1,35 +1,26 @@
 class Space:
-    def __init__(self, space_number, space_type, properties=None):
+    def __init__(self, space_number, space_type):
         self.space_number = space_number
         self.space_type = space_type
-        self.properties = properties if properties else {}
 
 class Board:
     def __init__(self):
-        self.spaces = self.create_board()
+        self.spaces = []
+        self._initialize_board()
     
-    def create_board(self):
-        spaces = []
+    def _initialize_board(self):
         for i in range(1, 121):
             if i == 1:
-                spaces.append(Space(i, "Start", {"message": "Start your journey!"}))
-            elif i in {2, 14, 26, 38, 50, 62, 74, 86, 98, 110, 120}:
-                spaces.append(Space(i, "Career Choice"))
-            elif i in {3, 15, 27, 39, 51, 63, 75, 87, 99, 111}:
-                spaces.append(Space(i, "House Choice"))
-            elif i in {4, 16, 28, 40, 52, 64, 76, 88, 100, 112}:
-                spaces.append(Space(i, "Payday"))
-            elif i in {5, 17, 29, 41, 53, 65, 77, 89, 101, 113}:
-                spaces.append(Space(i, "Life Event"))
+                self.spaces.append(Space(i, "Start"))
             elif i == 120:
-                spaces.append(Space(i, "Retirement", {"message": "You've reached retirement!"}))
+                self.spaces.append(Space(i, "Retirement"))
+            elif i % 12 == 2:
+                self.spaces.append(Space(i, "Career Choice"))
+            elif i % 12 == 3:
+                self.spaces.append(Space(i, "House Choice"))
+            elif i % 12 == 4:
+                self.spaces.append(Space(i, "Payday"))
+            elif i % 12 == 5:
+                self.spaces.append(Space(i, "Life Event"))
             else:
-                spaces.append(Space(i, "Regular"))
-
-        return spaces
-    
-    def get_space_info(self, space_number):
-        if 1 <= space_number <= 120:
-            return vars(self.spaces[space_number - 1])
-        else:
-            return None
+                self.spaces.append(Space(i, "Regular"))
